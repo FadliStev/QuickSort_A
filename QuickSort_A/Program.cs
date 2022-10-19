6d45d3896a336c331a55cea1adefa0421e383e00
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-10-19 17:05:39 +0700

    membuat method untuk menukar element pada temporary

--- QuickSort_A/Program.cs
+++ QuickSort_A/Program.cs
@@ -42,6 +42,15 @@ namespace QuickSort_A
                 arr[i] = Int32.Parse(s1);
             }
         }
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
     }
 
     internal class Program
