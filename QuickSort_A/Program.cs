b7bc7a57cd71ffe28f9a8d232ebf1e873d1fcdc8
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-10-19 16:42:38 +0700

    membuat array of intergers to hold values

--- QuickSort_A/Program.cs
+++ QuickSort_A/Program.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace QuickSort_A
 {
+
+    class Program
+    {
+        //array of intergers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0;//number of comparasion
+        private int mov_count = 0;//number of data movements
+    }
+
     internal class Program
     {
         static void Main(string[] args)
