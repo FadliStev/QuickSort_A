252cd4ff5ddf3f0ba3db7d550de61b38cdc60537
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-10-19 16:47:09 +0700

    membuat method void untuk input element

--- QuickSort_A/Program.cs
+++ QuickSort_A/Program.cs
@@ -16,6 +16,20 @@ namespace QuickSort_A
 
         //Number of elements in array
         private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+        }
     }
 
     internal class Program
