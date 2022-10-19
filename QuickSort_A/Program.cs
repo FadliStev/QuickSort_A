2e74c93f24bcce1bb6387eb640538a63fc5ca4b1
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-10-19 17:17:32 +0700

    membuat perulangan untuk menentukan elemen yang lebih besar

--- QuickSort_A/Program.cs
+++ QuickSort_A/Program.cs
@@ -64,6 +64,19 @@ namespace QuickSort_A
 
             i = low + 1;
             j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //search for an element greater than pivot 
+                while ((arr [i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+            }
         }
     }
 
